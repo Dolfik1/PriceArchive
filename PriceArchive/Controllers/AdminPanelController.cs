7af95800ca3e19c9114ab89305caed7f700e15a1
Nikolay M <dolphinn@bk.ru>
2014-11-27 19:29:11 +0300

    Added: AdminPanel controller.

---
+++ PriceArchive/Controllers/AdminPanelController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace PriceArchive.Controllers
+{
+    public class AdminPanelController : Controller
+    {
+        public ActionResult IndexPanel()
+        {
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            ViewBag.Message = "Вход в админ-панель";
+
+            return View();
+        }
+
+        public ActionResult Users()
+        {
+            ViewBag.Message = "Пользователи.";
+
+            return View();
+        }
+
+        public ActionResult Shops()
+        {
+            ViewBag.Message = "Магазины.";
+
+            return View();
+        }
+    }
+}
