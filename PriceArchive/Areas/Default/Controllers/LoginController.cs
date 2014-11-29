e082901b2f08a861409ebc845965c68ae733439c
Nikolay M <dolphinn@bk.ru>
2014-11-29 21:27:21 +0300

    Login ajax form fixed.

--- PriceArchive/Areas/Default/Controllers/LoginController.cs
+++ PriceArchive/Areas/Default/Controllers/LoginController.cs
@@ -35,5 +35,26 @@ namespace PriceArchive.Areas.Default.Controllers
             Auth.LogOut();
             return RedirectToAction("Index", "Home");
         }
+
+        [HttpGet]
+        public ActionResult Ajax()
+        {
+            return View(new LoginView());
+        }
+
+        [HttpPost]
+        public ActionResult Ajax(LoginView loginView)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = Auth.Login(loginView.Email, loginView.Password, loginView.IsPersistent);
+                if (user != null)
+                {
+                    return View("_Ok");
+                }
+                ModelState["Password"].Errors.Add("Пароли не совпадают");
+            }
+            return View(loginView);
+        }
     }
 }
